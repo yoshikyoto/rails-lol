f3ca029c8927eccd6cca1d936c15595595fae8cb
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-06-02 10:13:31 +0900

    exec `rails generate graphql:install`

--- lol/config/routes.rb
+++ lol/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+  	# graphqlデバッグ用ライブラリgraphiqlを利用するためのルーティング
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  end
+
+  post '/graphql', to: 'graphql#execute'
   get '/', to: 'application#index'
 end
