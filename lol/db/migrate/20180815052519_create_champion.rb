8b1d59e32e1f690131a12983a4811765dbcd4dac
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-08-17 11:09:24 +0900

    fix primary key

--- lol/db/migrate/20180815052519_create_champion.rb
+++ lol/db/migrate/20180815052519_create_champion.rb
@@ -1,12 +1,12 @@
 class CreateChampion < ActiveRecord::Migration[5.1]
   def up
     create_table :champion, id: false do |t|
-      t.string :id
-      t.string :name_jp, index: true
-      t.string :version, index: true
-      t.text :body, limit: 65535
+      t.string :id, null: false
+      t.string :name_jp, index: true, null: false
+      t.string :version, index: true, null: false
+      t.text :body, limit: 65535, null: false
     end
-    execute "ALTER TABLE champion ADD PRIMARY KEY (id);"
+    execute "ALTER TABLE champion ADD PRIMARY KEY (id, version);"
   end
 
   def down
