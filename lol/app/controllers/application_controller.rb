efff0ec323bf2cafb0f4be87b3fba545ba4f541f
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-08-20 11:38:50 +0900

    add view

--- lol/app/controllers/application_controller.rb
+++ lol/app/controllers/application_controller.rb
@@ -2,9 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    repo = Lol::LolRepository.new
-    versions = repo.versions
-    # puts champions
-    render html: versions
   end
 end
