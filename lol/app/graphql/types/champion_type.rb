eb775b871d932ae73973c64788c2ec39768f6e0f
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-08-23 03:45:00 +0900

    つらい

--- lol/app/graphql/types/champion_type.rb
+++ lol/app/graphql/types/champion_type.rb
@@ -1,6 +1,18 @@
 Types::ChampionType = GraphQL::ObjectType.define do
   name 'Champion'
 
+  # GraphQLのクライアントApolloがAPIのレスポンスをキャッシュするが
+  # その際にidをキーとして利用してキャッシュするらしい
+  # （idがない場合は_idでキャッシュされるっぽい？）
+  # championのレスポンスはidが同じでもversionが違うということがあり、
+  # idをレスポンスに含めると、キャッシュ機構まわりでバグるので、
+  # id以外に、本当にuniqueな_idを返せるようにする
+  field :_id, types.String do
+    resolve -> (champion, args, ctx) {
+      return champion.id + champion.version
+    }
+  end
+
   field :id, types.String do
     description 'チャンピオンのID'
     resolve -> (champion, args, ctx) {
